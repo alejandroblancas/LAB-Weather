5b18a4bef0047de96499ebf8796bc233d74e61d3
Alejandro Gonzalez <alejandro.a.g.blancas@gmail.com>
2025-11-25 21:05:28 -0600

    feat: actualizacion a fase entregable

--- script.js
+++ script.js
@@ -9,16 +9,23 @@ async function fetchWeatherData( latitude, longitude) {
     console.log(data.elevation);
     console.log(data.current_weather);
     console.log(data.current_weather.temperature);
+    console.log(data.current_weather.windspeed);
     return data.current_weather;
 }
 // fetchWeatherData( 25.666815, -100.28233);
 async function handleFetchClick(){
     console.log("Boton fetch clickeado");
+    const button = document.getElementById("fetch-btn");
+    button.innerText = "Loading...";
+    button.disabled = true;
     const latitude = document.getElementById("latitude-input").value;
     const longitude = document.getElementById("longitude-input").value;
     const currentTemperature = document.getElementById("temp-display");
+    const currentWind = document.getElementById("wind-display");
 
     const currentWeather = await fetchWeatherData(latitude, longitude);
     currentTemperature.textContent = currentWeather.temperature;
-
+    currentWind.textContent = currentWeather.windspeed;
+    button.disabled = false; // Habilita el botón nuevamente
+    button.innerText = 'Get Weather';
 }
